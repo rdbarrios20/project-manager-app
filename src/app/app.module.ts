7664a72dd8115b0114b149a53772517c18dff1f7
Rafael Barrios <rdbarrios20@gmail.com>
2024-09-12 02:12:12 -0500

    Additing components to work projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProjectComponent } from './components/project/project.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProjectComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
