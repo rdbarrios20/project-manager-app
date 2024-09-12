96981d0421db0428916d698b85a9bc5c74f67804
Rafael Barrios <rdbarrios20@gmail.com>
2024-09-12 02:21:21 -0500

    Basic routes were enabled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProjectComponent } from './components/project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/login', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'project', component: ProjectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
